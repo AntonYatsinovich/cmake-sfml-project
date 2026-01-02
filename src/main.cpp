7e95f1678b6c4b4e4690163d2c2f61f497b3321a
Antoha <theunknown1001002@gmail.com>
2026-01-02 23:07:25 +0700

    alpha 0.1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,65 @@
+// Main.cpp
+
+#include <iostream>
+#include <random>
 #include <SFML/Graphics.hpp>
+
+// Генератор случайных чисел
+std::random_device rd; 
+std::mt19937 gen(rd());
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    // Создание окна
+    auto window = sf::RenderWindow(sf::VideoMode({600, 600}), "CMake SFML Project");
+    window.setFramerateLimit(60);
+
+    // Создание круга
+    sf::CircleShape circle(30);
+    circle.setPosition({300, 300});
+
+    // Определяем диапазон случайных чисел
+    std::uniform_real_distribution<float> dist(0, window.getPosition().x - (circle.getRadius() * 2));
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
+            // Если игрок закроет игру, она завершится
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
             }
-        }
+            
+            // Если игрок нажмёт Escape, игра завершится
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+                    window.close();
+                }
+            }
 
-        window.clear();
+            // Проверка на попадание мышкой по кругу
+            else if (const auto* mouse = event->getIf<sf::Event::MouseButtonPressed>()) {
+                if (mouse->button == sf::Mouse::Button::Left) {
+                    // Вычисление вектора от центра круга до позиции мышки
+                    sf::Vector2f diff = sf::Vector2f(mouse->position) - (circle.getPosition() + sf::Vector2f(circle.getRadius(), circle.getRadius()));
+
+                    if (diff.lengthSquared() <= circle.getRadius() * circle.getRadius())
+                    {
+                        std::cout << "Попал!" << std::endl;
+                        circle.setPosition({dist(gen), dist(gen)});
+                    }
+                }
+            }
+        }
+        
+
+        window.clear(sf::Color::Black);
+
+        window.draw(circle);
+
         window.display();
     }
+
+    return 0;
 }
